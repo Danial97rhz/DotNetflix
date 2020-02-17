5052a81fbd54874ee87fd42c4140eebecdecf878
Danial <danial97rhz@outlook.com>
2020-02-17 13:43:32 +0100

    Added models

---
+++ DotNetflix.API/Models/MovieGenres.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetflix.API.Models
+{
+    public class MovieGenres
+    {
+        public string MovieId { get; set; }
+        public Movie Movie { get; set; }
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
---
+++ DotNetflix.API/Models/StoryLine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetflix.API.Models
+{
+    public class StoryLine
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
