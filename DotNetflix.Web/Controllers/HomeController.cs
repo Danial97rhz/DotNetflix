ec00a36bcf7bee14b23c383e55216232324f3c82
erikwahlstrom <54457926+erikwasa@users.noreply.github.com>
2020-02-23 01:42:02 +0100

    Dropped OMDB API, references and

--- DotNetflix.Web/Controllers/HomeController.cs
+++ DotNetflix.Web/Controllers/HomeController.cs
@@ -6,27 +6,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DotNetflix.Web.Models;
-using DotNetflix.API.Services;
-using Omdb.API;
 
 namespace DotNetflix.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly IMailService _mailService;
-        private readonly IMovieRepository _repository;
-        private readonly IOmdbRepository _omdbRepository;
 
-        public HomeController(ILogger<HomeController> logger, 
-            IMailService mailService,
-            IMovieRepository repository,
-            IOmdbRepository omdbRepository)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _mailService = mailService;
-            _repository = repository;
-            _omdbRepository = omdbRepository;
         }
 
         public IActionResult Index()
@@ -37,19 +26,6 @@ namespace DotNetflix.Web.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        
-
-
-
-        // SEARCH Omdb
-        public async Task<IActionResult> Search(string q)
-        {
-            var result = await _omdbRepository.Search(q);
-            ViewData["Search"] = q;
-
-            return View(result);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
