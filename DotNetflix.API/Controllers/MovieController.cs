82327092f5850844a3815e24ff5cd33f4dda5f08
Danial <danial97rhz@outlook.com>
2020-02-27 16:03:48 +0100

    OMDB API added + insert to local databse

--- DotNetflix.API/Controllers/MovieController.cs
+++ DotNetflix.API/Controllers/MovieController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 using AutoMapper;
 using DotNetflix.API.ModelsDto;
 using DotNetflix.API.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
 
 namespace DotNetflix.API.Controllers
 {
@@ -41,7 +45,7 @@ namespace DotNetflix.API.Controllers
         }
 
         [HttpGet("{movieId}")]
-        public ActionResult<MovieDto> GetMovie(string movieId)
+        public async Task<ActionResult<MovieDto>> GetMovieAsync(string movieId)
         {
             var movie = movieRepository.GetMovie(movieId);
             if (movie == null)
@@ -49,11 +53,57 @@ namespace DotNetflix.API.Controllers
                 return NotFound("Movie not found");
             }
 
-            //if (movie.PosterUrl == null)
-            //{
-            //    GetMovieDetailsFrom OMDBAPI
-            //}
-            return Ok(movie);
+            else 
+            {
+                    //Check if data is already saved in local databse
+                if (movie.PosterUrl == null || movie.LongPlot == null)
+                {
+                        var httpClient = new HttpClient();
+                        var omdbApiUrl = "http://www.omdbapi.com/?s=&apiKey=";
+                        var key = "dd0fa8bc";
+                        var uriBuilder = new UriBuilder(omdbApiUrl);
+                        var queryString = HttpUtility.ParseQueryString(uriBuilder.Query);
+                        queryString.Set("i", movieId);
+                        queryString.Set("apiKey", key);
+                        uriBuilder.Query = queryString.ToString();
+
+                        HttpResponseMessage httpResponse = await httpClient.GetAsync(uriBuilder.Uri);
+                        MovieDto response = new MovieDto();
+                        var json = await httpResponse.Content.ReadAsStringAsync();
+                        response = JsonConvert.DeserializeObject<MovieDto>(json);
+
+                        movie.PosterUrl = response.PosterUrl;
+                        movie.LongPlot = response.LongPlot;
+                        movie.Director = response.Director;
+                        movie.ReleaseDate = Convert.ToDateTime( response.ReleaseDate);
+                        movie.Actors = response.Actors.Split(',').ToList();
+
+                    //TODO: Fix this MESS
+                    using (SqlConnection con = new SqlConnection("Server=(localdb)\\MSSQLLocalDB;Database=DotNetflixDb;Trusted_Connection=True;"))
+                    {
+                        con.Open();
+                        SqlCommand command = new SqlCommand("insert into [MoviesDetails] (PosterUrl, Actors, ReleaseDate, Director, LongPlot) VALUES (@PosterUrl, @Actors ,@ReleaseDate , @Director , @LongPlot)" +
+                            "UPDATE Movies SET MoviesDetailsId = (select top 1 id from MoviesDetails order by id desc) WHERE MovieId=@movieId", con);
+                        command.Parameters.AddWithValue("@movieId", movieId);
+                        command.Parameters.AddWithValue("@PosterUrl", response.PosterUrl);
+                        command.Parameters.AddWithValue("@Actors", response.Actors);
+                        command.Parameters.AddWithValue("@ReleaseDate", response.ReleaseDate);
+                        command.Parameters.AddWithValue("@Director", response.Director);
+                        command.Parameters.AddWithValue("@LongPlot", response.LongPlot);
+                        command.ExecuteNonQuery();
+                        con.Close();
+                    }
+
+                }
+
+                //if (movie.PosterUrl == null)
+                //{
+                //    GetMovieDetailsFrom OMDBAPI
+                //}
+                return Ok(movie);
+            }
+
+            
         }
     }
 }
