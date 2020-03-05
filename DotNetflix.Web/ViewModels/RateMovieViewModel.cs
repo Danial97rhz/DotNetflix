d36ed5fe17e050d0b6262c4f150446697358cb50
erikwahlstrom <54457926+erikwasa@users.noreply.github.com>
2020-03-05 23:51:44 +0100

    Model, Controller for UserMovie(not implemented), RateMovieView. RatedMoviesView(not implemented)

---
+++ DotNetflix.Web/ViewModels/RateMovieViewModel.cs
@@ -0,0 +1,19 @@
+﻿using DotNetflix.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetflix.Web.ViewModels
+{
+    public class RateMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public int UserId { get; set; }
+        public string MovieId { get; set; }
+        [Range(1, 10)]
+        public int Rating { get; set; }
+        public string Review { get; set; }
+    }
+}
