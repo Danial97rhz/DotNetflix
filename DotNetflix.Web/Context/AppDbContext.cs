8c19bf4f1b76f0c776638547197ec4ed44008010
Bragde <47227297+Bragde@users.noreply.github.com>
2020-03-16 15:15:39 +0100

    changing Identity primary key to int

--- DotNetflix.Web/Context/AppDbContext.cs
+++ DotNetflix.Web/Context/AppDbContext.cs
@@ -5,12 +5,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DotNetflix.Web.Context
 {
-    public class AppDbContext : IdentityDbContext<ApplicationUser, IdentityRole, int>
+    public class AppDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
     { 
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // IDENTITY: Call base class OnModelCreating
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
