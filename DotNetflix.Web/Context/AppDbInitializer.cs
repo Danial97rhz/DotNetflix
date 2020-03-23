b82a5d55e70f6f90c3ab7b5a1b2315a8fcf2413f
erikwahlstrom <54457926+erikwasa@users.noreply.github.com>
2020-03-23 23:58:16 +0100

    User seed for 3 normal users

--- DotNetflix.Web/Context/AppDbInitializer.cs
+++ DotNetflix.Web/Context/AppDbInitializer.cs
@@ -11,12 +11,13 @@ namespace DotNetflix.Web.Context
     {
         public static void SeedUser(UserManager<ApplicationUser> userManager)
         {
-            if (userManager.FindByEmailAsync("admin@dotnetflix.com").Result==null)
+            if (userManager.FindByEmailAsync("admin@dotnetflix.com").Result == null)
             {
                 ApplicationUser user = new ApplicationUser()
                 {
                     UserName = "admin",
-                    Email = "admin@dotnetflix.com"
+                    Email = "admin@dotnetflix.com",
+                    EmailConfirmed = true
                 };
 
                 IdentityResult result = userManager.CreateAsync(user, "Password1!").Result;
@@ -24,6 +25,54 @@ namespace DotNetflix.Web.Context
                 if (result.Succeeded)
                 {
                     userManager.AddToRoleAsync(user, "Administrator").Wait();
+                }
+            }
+            if (userManager.FindByEmailAsync("erik@dotnetflix.com").Result == null)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UserName = "Erik",
+                    Email = "erik@dotnetflix.com",
+                    EmailConfirmed = true
+                };
+
+                IdentityResult result = userManager.CreateAsync(user, "Password1!").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "User").Wait();
+                }
+            }
+            if (userManager.FindByEmailAsync("erica@dotnetflix.com").Result == null)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UserName = "Erica",
+                    Email = "erica@dotnetflix.com",
+                    EmailConfirmed = true
+                };
+
+                IdentityResult result = userManager.CreateAsync(user, "Password1!").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "User").Wait();
+                }
+            }
+            if (userManager.FindByEmailAsync("daniel@dotnetflix.com").Result == null)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UserName = "Daniel",
+                    Email = "daniel@dotnetflix.com",
+                    EmailConfirmed = true
+                };
+
+                IdentityResult result = userManager.CreateAsync(user, "Password1!").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "User").Wait();
                 }
             }
         }
